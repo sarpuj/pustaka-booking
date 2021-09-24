108fcbbb9cbbfa53c256eb7ba80a80dc13b47fac
sarpuj <sarapujakesuma31@gmail.com>
2021-09-24 19:51:58 +0700

    penambahan Contoh1.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Sara Puja<br>
+        Saya tinggal di Yogyakarta<br>
+        Saya anak kedua dari tiga bersaudara<br>
+        Saya suka menghabiskan waktu luang saya dengan bermain musik atau memasak<br>
+        Cita-cita saya didunia adalah memiliki banyak uang."
+    }
+}
